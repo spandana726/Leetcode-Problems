861c1c6b4bda585616a67188bc8c2cd2d591cb5b
Gunaganti Spandana <141220204+spandana726@users.noreply.github.com>
2026-08-11 23:24:22 +0530

    Time: 1100 ms (80.49%), Space: 19.8 MB (11.64%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -0,0 +1,43 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        empty = []
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    empty.append((r, c))
+                else:
+                    num = board[r][c]
+                    rows[r].add(num)
+                    cols[c].add(num)
+                    boxes[(r // 3) * 3 + c // 3].add(num)
+
+        def solve(pos):
+            if pos == len(empty):
+                return True
+
+            r, c = empty[pos]
+            b = (r // 3) * 3 + c // 3
+
+            for num in '123456789':
+                if num not in rows[r] and num not in cols[c] and num not in boxes[b]:
+                    board[r][c] = num
+                    rows[r].add(num)
+                    cols[c].add(num)
+                    boxes[b].add(num)
+
+                    if solve(pos + 1):
+                        return True
+
+                    board[r][c] = '.'
+                    rows[r].remove(num)
+                    cols[c].remove(num)
+                    boxes[b].remove(num)
+
+            return False
+
+        solve(0)
