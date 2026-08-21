3d211d6b1326c56632d59b71e2a35d9857fee035
Gunaganti Spandana <141220204+spandana726@users.noreply.github.com>
2026-08-21 23:10:48 +0530

    Time: 23 ms (20.59%), Space: 21.9 MB (26.3%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -1,18 +1,23 @@
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
         n = len(customers)
+        i = 0
         count = 0
         ans = 0
+        ng = 0
         for k in range(0,n):
             if grumpy[k]==0:
                 count+=customers[k]
-        for i in range(0,n-minutes+1):
-            ng = 0
-            for j in range(i,i+minutes):
-                if grumpy[j]==1:
-                    ng+=customers[j]
-            ans = max(ans,ng)
-        count+=ans
-        return count
+        for j in range(0,n):
+            if grumpy[j]==1:
+                ng+=customers[j]
+            if j-i+1>minutes:
+                if grumpy[i]==1:
+                    ng-=customers[i]
+                i+=1
+            if j-i+1==minutes:
+                ans = max(ans,ng)
+        return count+ans
+
 
         
